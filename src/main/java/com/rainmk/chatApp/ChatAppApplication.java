b47e89e60e2b89d1bc82e04a4987ae84b7c9c9c2
rainmasak <rain.masak@hotmail.com>
2018-12-02 23:28:48 +0200

    Stuff

--- src/main/java/com/rainmk/chatApp/ChatAppApplication.java
+++ src/main/java/com/rainmk/chatApp/ChatAppApplication.java
@@ -2,11 +2,19 @@ package com.rainmk.chatApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChatAppApplication {
+public class ChatAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatAppApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChatAppApplication.class);
+    }
+	
 }
